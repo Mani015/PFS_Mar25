a8072617c6851710bd2bfe661398d8d404814dfd
mani015 <ramanadhulamanikumar01975@gmail.com>
2025-05-22 15:49:04 +0530

    module

---
+++ Python/Day32(Modules)/Time_Module/ex3.py
@@ -0,0 +1,12 @@
+import time
+a="my name is sonia"
+print(a)
+time.sleep(2)
+v="my name is suresh"
+print(v)
+time.sleep(5)
+c="my name is yogeswar"
+print(c)
+d = "shiva"
+time.sleep(2)
+print(d)
---
+++ Python/Day32(Modules)/Time_Module/ex4.py
@@ -0,0 +1,17 @@
+# calendar
+import calendar
+c1=calendar.prcal(2025)
+print(c1)
+
+import datetime
+
+# datetime.date(Y, M, D)
+d = datetime.date(2025, 5, 22)
+print(d)
+print(d.day)
+print(d.month)
+print(d.year)
+# 2025-05-22
+# 22
+# 5
+# 2025
